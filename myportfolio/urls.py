144ab07b45cbf5c0581564d0e6b7c10aa5275ca8
Manmeet Singh <manmeetsingh28603@gmail.com>
2025-07-29 20:46:16 +0530

    Fix favicon issues: Add favicon.ico and favicon.png files with proper routing

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/static/images/favicon.ico', permanent=True)),
+    path('favicon.png', RedirectView.as_view(url='/static/images/favicon.png', permanent=True)),
 ]
+
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
